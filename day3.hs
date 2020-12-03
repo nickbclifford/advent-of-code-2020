cd42f224a2ced3ba7b2cd412a06c5ecca76e669b
Nick Clifford <nick@nickclifford.me>
2020-12-03 12:05:24 -0600

    actually I don't need a reader

--- day3.hs
+++ day3.hs
@@ -1,48 +1,30 @@
-{-# LANGUAGE LambdaCase #-}
-
-import Control.Monad.Reader
 import Utils
 
-data Square = Open | Tree deriving Eq
+type Grid = [[Char]]
 
-fromChar :: Char -> Square
-fromChar = \case
-    '.' -> Open
-    '#' -> Tree
-
-isTree :: Square -> Integer
-isTree = \case
-    Open -> 0
-    Tree -> 1
-
-type Grid = [[Square]]
-type WithGrid = Reader Grid
-
-idxGrid :: Int -> Int -> WithGrid Square
-idxGrid row col = do
-    grid <- ask
+isTree :: Grid -> Int -> Int -> Integer
+isTree grid row col =
     let rowList = grid !! row
-    return $ rowList !! (col `mod` length rowList) -- mod to handle the "repeating"
+    in case rowList !! (col `mod` length rowList) of -- mod to handle the "repeating"
+        '.' -> 0
+        '#' -> 1
 
-stepCount :: Int -> Int -> Int -> Int -> WithGrid Integer
-stepCount down right row col = do
-    grid <- ask
+stepCount :: Grid -> Int -> Int -> Int -> Int -> Integer
+stepCount grid down right row col =
     let row' = row + down
         col' = col + right
-    square <- idxGrid row' col'
-    total <- stepCount down right row' col'
-    return $
-        if row' >= length grid -- stop once we reach the bottom
-        then 0
-        else isTree square + total
+    in if row' >= length grid -- stop once we reach the bottom
+       then 0
+       else isTree grid row' col' + stepCount grid down right row' col'
 
-countSlope :: Int -> Int -> WithGrid Integer
-countSlope right down = stepCount down right 0 0
+
+countSlope :: Grid -> Int -> Int -> Integer
+countSlope grid right down = stepCount grid down right 0 0
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
-    let grid = map (map fromChar) $ lines input
-        runPrint = print . flip runReader grid
-    runPrint $ countSlope 3 1
-    runPrint . fmap product . mapM (uncurry countSlope) $ [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    let grid = lines input
+        execSlope = countSlope grid
+    print $ execSlope 3 1
+    print . product . map (uncurry execSlope) $ [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
