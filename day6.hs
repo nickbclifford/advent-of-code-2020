e0f139429f183f8c7ad45930a167f4db19813f5e
Nick Clifford <nick@nickclifford.me>
2020-12-06 00:21:03 -0600

    that's how folds work, you troglodyte

--- day6.hs
+++ day6.hs
@@ -3,15 +3,9 @@ import qualified Data.Text.IO as TI
 import qualified Data.Set as S
 import Utils
 
-commonEls :: [S.Set Char] -> S.Set Char
-commonEls (x:xs) = 
-    if null xs
-    then x -- the common elements between a single set is just that set
-    else foldr S.intersection x xs
-
 main :: IO ()
 main = do
     input <- TI.readFile "input.txt"
     let textGroups = T.splitOn (T.pack "\n\n") input
     print . sum . map (length . S.fromList . filter (/= '\n') . T.unpack) $ textGroups
-    print . sum . map (length . commonEls . map S.fromList . lines . T.unpack) $ textGroups
+    print . sum . map (length . foldr1 S.intersection . map S.fromList . lines . T.unpack) $ textGroups
