8c2cbc35f82cfb9245267b9d759141fab3c5097a
Nick Clifford <nick@nickclifford.me>
2020-12-07 23:44:38 -0600

    cleanup

--- day8.hs
+++ day8.hs
@@ -1,3 +1,4 @@
+import Data.List
 import Data.Maybe
 import qualified Data.Set as S
 import Text.ParserCombinators.ReadP
@@ -5,6 +6,7 @@ import Utils
 
 data Instruction = Instruction String Int
 
+parseInstruction :: ReadP Instruction
 parseInstruction = do
     ins <- stringSize 3
     char ' '
@@ -16,31 +18,27 @@ parseInstruction = do
 instance Read Instruction where
     readsPrec _ = readP_to_S parseInstruction
 
-runUntilDup instructions (acc, pc, seen) =
-    if pc `S.member` seen
-    then acc
-    else case instructions !! pc of
-        Instruction "nop" _ -> runUntilDup instructions (acc, pc + 1, seen')
-        Instruction "acc" p -> runUntilDup instructions (acc + p, pc + 1, seen')
-        Instruction "jmp" p -> runUntilDup instructions (acc, pc + p, seen')
-        where seen' = S.insert pc seen
+data Behavior = Loops | Terminates deriving Eq
 
-doesTerminate instructions (acc, pc, seen) =
+runProgram :: [Instruction] -> (Int, Int, S.Set Int) -> (Int, Behavior)
+runProgram instructions (acc, pc, seen) =
     if pc >= length instructions
-    then Just acc
+    then (acc, Terminates)
     else
         if pc `S.member` seen
-        then Nothing
+        then (acc, Loops)
         else case instructions !! pc of
-            Instruction "nop" _ -> doesTerminate instructions (acc, pc + 1, seen')
-            Instruction "acc" p -> doesTerminate instructions (acc + p, pc + 1, seen')
-            Instruction "jmp" p -> doesTerminate instructions (acc, pc + p, seen')
+            Instruction "nop" _ -> runProgram instructions (acc, pc + 1, seen')
+            Instruction "acc" p -> runProgram instructions (acc + p, pc + 1, seen')
+            Instruction "jmp" p -> runProgram instructions (acc, pc + p, seen')
             where seen' = S.insert pc seen
 
+mutate :: Instruction -> Instruction
 mutate i@(Instruction "acc" _) = i
 mutate   (Instruction "jmp" p) = Instruction "nop" p
 mutate   (Instruction "nop" p) = Instruction "jmp" p
 
+mutateAt :: Int -> [Instruction] -> [Instruction]
 mutateAt i xs = case splitAt i xs of
     (first, ins:rest) -> first ++ (mutate ins):rest
     (first, []) -> first
@@ -49,5 +47,7 @@ main :: IO ()
 main = do
     input <- readFile "input.txt"
     let instructions = map read . lines $ input :: [Instruction]
-    print $ runUntilDup instructions (0, 0, S.empty)
-    print . catMaybes $ map (\i -> doesTerminate (mutateAt i instructions) (0, 0, S.empty)) [0..length instructions - 1]
+        initState = (0, 0, S.empty)
+        mutated = map (\i -> runProgram (mutateAt i instructions) initState) [0..length instructions - 1]
+    print . fst $ runProgram instructions initState
+    print . fst . fromJust . find ((== Terminates) . snd) $ mutated
