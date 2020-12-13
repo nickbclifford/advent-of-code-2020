3f0d894a6eddbd9d217be2629a3d630f5a31237c
Nick Clifford <nick@nickclifford.me>
2020-12-13 00:44:14 -0600

    implement CRT

--- day13.hs
+++ day13.hs
@@ -11,6 +11,11 @@ main = do
         time = read timestamp
         schedule = map ((\s -> if s == "x" then -1 else read s) . T.unpack) . T.splitOn "," . T.pack $ schedS
         ids = filter (/= -1) schedule
+        bigM = product ids
+        step (a, m) =
+            let b  = bigM `div` m
+                b' = (b ^ (m - 2)) `mod` m -- all our moduli are prime, so we can use a shortcut to find the modular inverse
+            in a * b * b'
     print . uncurry (*) . minimumBy (comparing snd) . map (\i -> (i, i - rem time i)) $ ids
-    print . filter ((/= -1) . snd) . zipWith (\w i -> (i - w, i)) [0..] $ schedule
-    -- just plug into a Chinese Remainder Theorem calculator online because honestly I am so angry about this
+    -- https://crypto.stanford.edu/pbc/notes/numbertheory/crt.html#_for_several_equations
+    print . (`mod` bigM) . sum . map step . filter ((/= -1) . snd) . zipWith (\w i -> (i - w, i)) [0..] $ schedule
