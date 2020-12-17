d16de5d425ff216b60ef1a801bbbadeb0f3b99fc
Nick Clifford <nick@nickclifford.me>
2020-12-17 01:05:42 -0600

    minor cleanup

--- day17.hs
+++ day17.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 import Data.Either
-import Data.Foldable
 import qualified Data.Map.Strict as M
 import Utils
 
@@ -9,10 +8,12 @@ class Ord a => Index a where
     neighbors :: a -> [a]
 
 instance Index (Int, Int, Int) where
-    neighbors (x, y, z) = [(x + dx, y + dy, z + dz) | dx <- [-1..1], dy <- [-1..1], dz <- [-1..1], (dx, dy, dz) /= (0, 0, 0)]
+    neighbors (x, y, z) = [(x + dx, y + dy, z + dz) |
+        dx <- [-1..1], dy <- [-1..1], dz <- [-1..1], (dx, dy, dz) /= (0, 0, 0)]
 
 instance Index (Int, Int, Int, Int) where
-    neighbors (x, y, z, w) = [(x + dx, y + dy, z + dz, w + dw) | dx <- [-1..1], dy <- [-1..1], dz <- [-1..1], dw <- [-1..1], (dx, dy, dz, dw) /= (0, 0, 0, 0)]
+    neighbors (x, y, z, w) = [(x + dx, y + dy, z + dz, w + dw) |
+        dx <- [-1..1], dy <- [-1..1], dz <- [-1..1], dw <- [-1..1], (dx, dy, dz, dw) /= (0, 0, 0, 0)]
 
 step :: Index a => M.Map a Char -> M.Map a Char
 step dim =
@@ -29,19 +30,18 @@ step dim =
             (idx, c) <- M.toList dim
             let (empty, ns) = neighborsAround idx
             Right (idx, next c ns) : map Left empty
-        nextUnknown = [(idx, if activeN == 3 then '#' else '.') |
-                idx <- unknown,
-                let (_, ns) = neighborsAround idx,
-                let activeN = count '#' ns]
+        nextUnknown = [(idx, if count '#' ns == 3 then '#' else '.') |
+            idx <- unknown,
+            let (_, ns) = neighborsAround idx]
     in M.fromList nextKnown <> M.fromList nextUnknown
 
 main :: IO ()
 main = do
     input <- readFile "input.txt"
     let list = lines input
-        len = length list
-        init3 = M.fromList [ ((x, y, 0 :: Int), list !! y !! x) | x <- [0..len - 1], y <- [0..len - 1]]
-        init4 = M.fromList [ ((x, y, 0 :: Int, 0 :: Int), list !! y !! x) | x <- [0..len - 1], y <- [0..len - 1]]
+        lenR = [0..length list - 1]
+        init3 = M.fromList [ ((x, y, 0 :: Int)          , list !! y !! x) | x <- lenR, y <- lenR]
+        init4 = M.fromList [ ((x, y, 0 :: Int, 0 :: Int), list !! y !! x) | x <- lenR, y <- lenR]
     -- do stuff
-    print . count '#' . toList $ nTimes step init3 6
-    print . count '#' . toList $ nTimes step init4 6
+    print . count '#' . M.elems $ nTimes step init3 6
+    print . count '#' . M.elems $ nTimes step init4 6
